dd713021783417745417658d4993218c754dcff8
Peter Krause <krausepeterm@gmail.com>
2022-02-21 14:58:22 -0300

    doc: Add @moduledoc to Scryfall.Serializer

--- lib/scryfall/serializer.ex
+++ lib/scryfall/serializer.ex
@@ -2,7 +2,8 @@ defmodule Scryfall.Serializer do
   import Map, only: [keys: 1, values: 1]
 
   @moduledoc """
-  TODO
+  The Serializer does convenient, repetitive work related to
+  struct-building used by other API modules.
   """
 
   @spec from_json(map, to: struct) :: struct
@@ -11,17 +12,11 @@ defmodule Scryfall.Serializer do
       object: object,
       has_more: has_more,
       next_page: nil,
-      data: serialize_data(data, base_struct)
+      data: data |> Enum.map(fn raw_data -> build_obj(raw_data, base_struct) end)
     }
   end
 
   def from_json(raw_map, to: base_struct), do: build_obj(raw_map, base_struct)
-
-  @spec serialize_data(list, struct) :: list
-  defp serialize_data(raw_content, base_struct) do
-    raw_content
-    |> Enum.map(fn raw_data -> build_obj(raw_data, base_struct) end)
-  end
 
   @spec build_obj(map, struct) :: struct
   defp build_obj(raw_map, base_struct) do
@@ -30,6 +25,7 @@ defmodule Scryfall.Serializer do
   end
 
   @spec attach_field(tuple, struct) :: struct
-  defp attach_field({field, value}, base_struct),
-    do: %{base_struct | String.to_atom(field) => value}
+  defp attach_field({field, value}, base_struct) do
+    %{base_struct | String.to_atom(field) => value}
+  end
 end
